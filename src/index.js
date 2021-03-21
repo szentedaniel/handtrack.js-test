d0d80ddfada32c13198008825cff439a7eb0fcf4
szentedaniel <karifull2000@gmail.com>
2021-03-22 00:46:15 +0100

    😐😐

---
+++ src/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const dotenv = require('dotenv').config();
+const socket = require('socket.io')
+
+
+
+const app = express();
+const server = http.createServer(app)
+
+io = socket(server);
+
+app.use(express.static(path.join(__dirname, '..', 'public')));
+const PORT = process.env.PORT || 8000;
+server.listen(PORT, () => {
+    console.log(`Listening at http://localhost:${PORT}`);
+})
+
+let allClients = [];
+
+io.on('connection', socket => {
+    console.log('Client connected', socket.id);
+    allClients.push(socket);
+    
+    socket.on('disconnect', () => {
+        console.log('1 user disconnected');
+        allClients.splice(allClients.indexOf(socket), 1);
+        
+    })
+})
