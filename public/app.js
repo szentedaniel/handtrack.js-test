12f5d4bed1af5727578a577bc1a23de249047dbe
szentedaniel <karifull2000@gmail.com>
2021-04-12 21:56:19 +0200

    😥😥

--- public/app.js
+++ public/app.js
@@ -1,134 +1,98 @@
-const socket = io();
+import { updateDarabok } from './darab.js'
+import { updateSonkak, szendvicshez, resetPalya } from './sonka.js'
+import { rendeles } from './rendeles.js'
 
-const video = document.querySelector('#video');
-const canvas = document.querySelector('#canvas');
-const context = canvas.getContext('2d');
-let model;
-
-
-
-
-navigator.getUserMedia_ = (navigator.getUserMedia ||
-    navigator.webkitGetUserMedia ||
-    navigator.mozGetUserMedia ||
-    navigator.msGetUserMedia);
-
-const modelParams = {
-    flipHorizontal: true, // flip e.g for video 
-
-    //imageScaleFactor: 0.7, // reduce input image size for (maybe) gains in speed.
-    maxNumBoxes: 1, // maximum number of boxes to detect
-    iouThreshold: 0.5, // ioU threshold for non-max suppression
-    scoreThreshold: 0.8, // confidence threshold for predictions.
-}
-
-handTrack.load(modelParams).then(newModel => {
-    model = newModel
-});
-
-handTrack.startVideo(video).then((status) => {
-    if (status) {
-        videoOn = false;
-        setInterval(() => {
-            runDetection()
-        }, 1000 / 30);
-        //console.log(status);
-    } else {
-        console.log("Please enable video")
+let app=new PIXI.Application(
+    {
+        backgroundColor: 0xAAAAAA
     }
-});
+);
+app.renderer.resize(window.innerWidth-50, window.innerHeight-80);
 
-const knife = {}
+document.body.appendChild(app.view);
+app.stage.interactive=true;
 
-let handX = 0;
-let handY = 0;
-const kes = document.createElement('div')
-kes.style.position = 'relative'
-kes.textContent = '🔪'
-kes.style.fontSize = '5em'
-document.body.appendChild(kes)
+let szint=1;
+document.getElementById("kovetkezo").onclick= kovetkezoPalya;
+document.getElementById("ujra").onclick=ujra;
 
-const runDetection = () => {
-    model.detect(video).then(predictions => {
-        //model.renderPredictions(predictions, canvas, context, video);
-        //console.log(predictions);
-        //requestAnimationFrame(runDetection);
-        if (predictions.length !== 0) {
-            let hand1 = predictions[0].bbox;
-            handX = hand1[0];
-            handY = hand1[1];
-            //console.log(handX, handY);
-            moveMouse(handX, handY)
+let pointd=false;
+app.view.setAttribute("id", 'game');
+document.addEventListener('pointerdown', function() {pointd=true;});
+document.addEventListener('pointerup', function() {pointd=false;});
+document.getElementById("display").innerHTML=(
+    "csirke:\t"+szendvicshez[0]+
+    " hagyma:\t"+szendvicshez[1]+
+    " paprika:\t"+szendvicshez[2]+
+    " paradicsom:\t"+szendvicshez[3]+
+    " párizsi:\t"+szendvicshez[4]+
+    " sajt:\t"+szendvicshez[5]+
+    " saláta:\t"+szendvicshez[6]+
+    " sonka:\t"+szendvicshez[7]+
+    " uborka:\t"+szendvicshez[8]);
 
-            // socket.emit('hand-motion', {
-            //     id: socket.id,
-            //     x: x,
-            //     y: y
-            // })
-        }
-    });
-}
-let coords = [{}]
+/*
+    let counter=new PIXI.Text("csirke:\t"+szendvicshez[0]+
+    " hagyma:\t"+szendvicshez[1]+
+    " paprika:\t"+szendvicshez[2]+
+    " paradicsom:\t"+szendvicshez[3]+
+    " párizsi:\t"+szendvicshez[4]+
+    " sajt:\t"+szendvicshez[5]+
+    " saláta:\t"+szendvicshez[6]+
+    " sonka:\t"+szendvicshez[7]+
+    " uborka:\t"+szendvicshez[8],
+    {
+        fontFamily : 'Arial',
+        fontSize: 24,
+        fill : 0xff1010,
+        align : 'center'
+    },
+    app.view)
+    */
 
-document.addEventListener("handevent", (e) => {
-    const x = e.detail.clientY / video.height * window.innerHeight
-    const y = e.detail.clientX / video.width * window.innerWidth
-    if (coords.length <=5) {
-        coords.push({x: x, y: y})
-    }else {
-        coords.shift()
-        coords.push({x: x, y: y})
-    }
-    let sumX = 0
-    let sumY = 0
-    coords.forEach(cord => {
-        sumX += cord.x
-        sumY += cord.y
-    });
-    
-    kes.style.top = sumX/5 + 'px'
-    kes.style.left = sumY/5 + 'px'
-    //console.log(e.detail.clientX, e.detail.clientY);
-})
+const loader=PIXI.Loader.shared;
+loader.add("sonka_spritesheet","spritesheets/sonka2.json").load(setup);
 
-
-// socket.on('hand-motion', data => {
-//     console.log(data.id, '\n', data.x, data.y);
-//     let knife_ = knife[data.id]
-//     if(!knife_){
-//         const div = document.createElement('div')
-//         div.style.position= 'relative'
-//         div.textContent = '🔪'
-//         div.style.fontSize = '5em'
-//         knife[data.id] = div
-//         knife_ = div
-//         document.body.appendChild(div)
-//     }
-//     const x = data.y/video.height * window.innerHeight
-//     const y = data.x/video.width * window.innerWidth
-
-
-//     //knife_.style.top = x + 'px'
-//     //knife_.style.left = y + 'px'
-
-// })
-
-const moveMouse = (x, y) => {
-    const event = new CustomEvent("handevent", { //MouseEvent-mousemove
-        bubbles: true,
-        cancelable: true,
-        detail: {
-            clientX: x,
-            clientY: y
-        }
-    });
-    document.dispatchEvent(event)
+window.onresize = function(){
+    app.renderer.resize(window.innerWidth-50, window.innerHeight-80);
 }
 
+function setup(){
+/*    //animáció
+    let sonka_sheet=loader.resources["sonka_spritesheet"].spritesheet;
 
+    animatedsprite=new PIXI.AnimatedSprite(sonka_sheet.animations["sonka"]);
+    animatedsprite.animationSpeed=0.5;
+    animatedsprite.loop=false;
+    animatedsprite.x=100;
+    animatedsprite.y=0;
 
+    //animatedsprite.play();
+    app.stage.addChild(animatedsprite);
+    */
 
+    app.ticker.add(updateSonkak);
+    app.ticker.add(updateDarabok);
 
-// const noiseX = (noise.simplex2(0, a*0.0005) + 1) / 2;
-//   // We get another noise value for the y axis but because we don't want the same value than x, we need to use another value for the first parameter
-// const noiseY = (noise.simplex2(1, a*0.0005) + 1) / 2;
+    rendeles(szint);
+}
+
+function kovetkezoPalya(){
+    szint++;
+    rendeles(szint);
+    resetPalya();
+}
+
+function ujra(){
+    rendeles(szint);
+    resetPalya();
+}
+
+/*
+function animationLoop(){
+    requestAnimationFrame(animationLoop);
+    animatedsprite.rotation+=0.03;
+    animatedsprite.y+=1;
+}*/
+
+export { app, pointd }
---
+++ public/tracking.js
@@ -0,0 +1,135 @@
+const socket = io();
+
+const video = document.querySelector('#video');
+const canvas = document.querySelector('#canvas');
+const context = canvas.getContext('2d');
+let model;
+
+
+
+
+navigator.getUserMedia_ = (navigator.getUserMedia ||
+    navigator.webkitGetUserMedia ||
+    navigator.mozGetUserMedia ||
+    navigator.msGetUserMedia);
+
+const modelParams = {
+    flipHorizontal: true, // flip e.g for video 
+
+    //imageScaleFactor: 0.7, // reduce input image size for (maybe) gains in speed.
+    maxNumBoxes: 1, // maximum number of boxes to detect
+    iouThreshold: 0.5, // ioU threshold for non-max suppression
+    scoreThreshold: 0.8, // confidence threshold for predictions.
+}
+
+handTrack.load(modelParams).then(newModel => {
+    model = newModel
+});
+
+handTrack.startVideo(video).then((status) => {
+    if (status) {
+        //videoOn = false;
+        setInterval(() => {
+            runDetection()
+        }, 1000 / 30);
+        //console.log(status);
+    } else {
+        console.log("Please enable video")
+    }
+});
+
+const knife = {}
+
+let handX = 0;
+let handY = 0;
+// const kes = document.createElement('div')
+// kes.style.position = 'relative'
+// kes.textContent = '🔪'
+// kes.style.fontSize = '5em'
+// document.body.appendChild(kes)
+
+const runDetection = () => {
+    model.detect(video).then(predictions => {
+        //model.renderPredictions(predictions, canvas, context, video);
+        //console.log(predictions);
+        //requestAnimationFrame(runDetection);
+        if (predictions.length !== 0) {
+            let hand1 = predictions[0].bbox;
+            handX = hand1[0];
+            handY = hand1[1];
+            console.log('tracking: ', handX, handY);
+            
+            //moveMouse(handX, handY)
+
+            // socket.emit('hand-motion', {
+            //     id: socket.id,
+            //     x: x,
+            //     y: y
+            // })
+        }
+    });
+}
+let coords = [{}]
+
+document.addEventListener("handevent", (e) => {
+    const x = e.detail.clientY / video.height * window.innerHeight
+    const y = e.detail.clientX / video.width * window.innerWidth
+    if (coords.length <=5) {
+        coords.push({x: x, y: y})
+    }else {
+        coords.shift()
+        coords.push({x: x, y: y})
+    }
+    let sumX = 0
+    let sumY = 0
+    coords.forEach(cord => {
+        sumX += cord.x
+        sumY += cord.y
+    });
+    
+    // kes.style.top = sumX/5 + 'px'
+    // kes.style.left = sumY/5 + 'px'
+    //console.log(e.detail.clientX, e.detail.clientY);
+})
+
+
+// socket.on('hand-motion', data => {
+//     console.log(data.id, '\n', data.x, data.y);
+//     let knife_ = knife[data.id]
+//     if(!knife_){
+//         const div = document.createElement('div')
+//         div.style.position= 'relative'
+//         div.textContent = '🔪'
+//         div.style.fontSize = '5em'
+//         knife[data.id] = div
+//         knife_ = div
+//         document.body.appendChild(div)
+//     }
+//     const x = data.y/video.height * window.innerHeight
+//     const y = data.x/video.width * window.innerWidth
+
+
+//     //knife_.style.top = x + 'px'
+//     //knife_.style.left = y + 'px'
+
+// })
+
+const moveMouse = (x, y) => {
+    const event = new CustomEvent("handevent", { //MouseEvent-mousemove
+        bubbles: true,
+        cancelable: true,
+        detail: {
+            clientX: x,
+            clientY: y
+        }
+    });
+    document.dispatchEvent(event)
+}
+
+
+export {handX, handY}
+
+
+// const noiseX = (noise.simplex2(0, a*0.0005) + 1) / 2;
+//   // We get another noise value for the y axis but because we don't want the same value than x, we need to use another value for the first parameter
+// const noiseY = (noise.simplex2(1, a*0.0005) + 1) / 2;
